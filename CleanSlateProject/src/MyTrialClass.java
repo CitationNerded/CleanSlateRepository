612fd8d15d2fd9322fe743de78c12d7cb9ec99e3
17mibe-IRD <michaelabeswick@gmail.com>
2017-02-08 10:55:18 +1300

    A Commit

---
+++ CleanSlateProject/src/MyTrialClass.java
@@ -0,0 +1,32 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MyTrialClass {
+
+	public static void main(String []args){
+
+		List<Integer> fibList = new ArrayList<Integer>();
+		fibList.add(1);
+		fibList.add(1);
+		fibList.add(2);
+
+		for (int n =1; n<=10; n++) {
+			fibList.get(fibList.size()-2);
+			int firstNumber =  fibList.get(fibList.size()-2);
+			fibList.get(fibList.size()-1);
+			int secondNumber = fibList.get(fibList.size()-1);
+	
+			int sumNumber = firstNumber+secondNumber;
+			fibList.add(sumNumber);
+			//System.out.println(firstNumber);
+			//System.out.println(secondNumber);
+			System.out.println(sumNumber);
+			//  System.out.println("Element "+n+" at index 0: " + fibonacciArray[n]);
+			//fibonacciArray.toArray(sumNumber);
+			// second
+		    }
+	}
+}
